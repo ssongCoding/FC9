bd8583d5b9d2f3ff1f2867a2fb86a5847aaa8e46
ssongCoding <songa.kim@kakao.com>
2022-06-28 00:26:37 +0900

    초기 커밋.

---
+++ day02.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*
+Ctrl + F5 : 디버깅하지 않고 시작
+F5 : 디버깅 후 시작
+Ctrl + k + c : 해당 줄 주석 처리
+Ctrl + k + u : 해당 줄 주석 해제
+*/
+
+int main(void) {
+	// 01. 복습
+	printf("hello");
+
+	// 02.
+	int number;
+	number = 7;
+	printf("%d", number); // decimal; 10진수
+
+	return 0;
+}
